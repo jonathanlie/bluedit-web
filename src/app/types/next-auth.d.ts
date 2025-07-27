6bc98ac6b42bcb5322f9317eed46f9a7115a9878
jonathanlie <jonathanlie94@gmail.com>
2025-07-28 06:22:34 +0800

    Finish basic sign in functionality

---
+++ src/app/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    id_token?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    id_token?: string;
+  }
+}
